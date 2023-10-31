82196274f8cbf64cf8b77d8590a7789476a5aa3f
mikeninerbravog <bravog.bot2@gmail.com>
2023-10-31 12:46:04 -0300

    commit

---
+++ python/s1.py
@@ -0,0 +1,15 @@
+# Exchange Rate API
+# Get your Free API: https://currencyapi.com/docs
+
+import requests as req
+
+api_key = 'cur_live_aK34mQHiXdsY4iHXBugHrm8sHwybWQg6Prc5S4Xx'
+
+api_url = 'https://api.currencyapi.com/v3/latest?apikey='
+
+result = req.get(api_url + api_key)
+
+data = result.json()["data"]
+
+for currency in data:
+    print(currency, " : ", data[currency]["value"])
