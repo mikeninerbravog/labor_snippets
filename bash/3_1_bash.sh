706331f2a2e374969502377dab7e1262c037c25c
mikeninerbravog <bravog.bot2@gmail.com>
2023-11-01 08:09:21 -0300

    bora commit

---
+++ bash/3_1_bash.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Code blocks and I/O redirection
+# Reading lines in local file
+
+File=dumb1
+
+{
+read line1
+read line2
+} < $File
+
+echo "First line in $File is:"
+echo "$line1"
+echo
+echo "Second line in $File is:"
+echo "$line2"
+
+exit 0
+
+# Now, how do you parse the separate fields of each line?
+# Hint: use awk, or . . .
+# . . . Hans-Joerg Diers suggests using the "set" Bash builtin
