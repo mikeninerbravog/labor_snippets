423e211dc61eb004afe350d272d22d724410ee0c
Mike Niner Bravog <37249508+mikeninerbravog@users.noreply.github.com>
2023-11-05 23:27:52 -0300

    Delete pass.sh

--- pass.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-n=$1  # Replace with the number of days ago you want
-
-new_date="$(date -d "@$(( $(date +%s) - n * 86400 ))" "+%Y-%m-%d %H:%M:%S")"
-
-GIT_COMMITTER_DATE="$new_date" git commit --amend --no-edit
-
